f669c498cff5675c35ec0bf010c6d2ab43615eec
thanhan7914 <thanhan7914@gmail.com>
2016-11-14 15:39:23 +0700

    new :))

--- router/dashboard/newfeed.js
+++ router/dashboard/newfeed.js
@@ -2,11 +2,39 @@ const router = require('express').Router();
 const Query = require('mongo-promise');
 const utils = require('../../utils');
 
-router.get('/', function(req, res) {
+router.get('/', function(req, res, next) {
+  if(req.url === '/?newposts')
+    return next();
+
   let options = {};
   options.username = req.session.user.username;
 
-  res.render('dashboard/newfeed', options);
+  let count;
+
+  let db = new Query(utils.config.dbname);
+  db.query({}, 'newfeed')
+  .exec('count')
+  .handle((c) => {
+    count = c;
+  })
+  .query({}, 'newfeed')
+  .exec('limit', utils.config.dashboard.newfeed.limit)
+  .exec('toArray')
+  .handle((docs) => {
+    //query
+  })
+  .except((error) => {
+    //error
+  })
+  .close()
+  .handle((done) => {
+    res.render('dashboard/newfeed', options);
+  });
+});
+
+router.use(function(req, res) {
+  console.log('ext');
+  res.render('dashboard/newposts');
 });
 
 module.exports = router;
