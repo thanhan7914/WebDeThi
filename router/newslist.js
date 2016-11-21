4e1efe0ef9feb58f6cfe5ef5bf4792437a120d31
thanhan7914 <thanhan7914@gmail.com>
2016-11-21 23:13:14 +0700

    up

--- router/newslist.js
+++ router/newslist.js
@@ -26,21 +26,76 @@ router.get('/newslist', function(req, res) {
     for(let i = 0; i < rows.length; i++)
     {
       let href = `/readnews/${utils.toUrl(rows[i].title)}.${rows[i]._id}`;
+      if(i === 0)
+      {
+        options.topnews = `
+        <div class="news-body-main">
+          <div class="images-news">
+            <a href="readnews">
+              <img class="img-responsive" src="${rows[i].image}" alt="${rows[i].title}" />
+            </a>
+          </div>
 
-      options.news += `
-        <div class="row" style="padding: 4px 2px; border-top: 1px solid #eee;">
-          <div class="col-md-4">
-            <div class="img-news img-newsfeed" style="width: 100%">
-              <a href="${href}"><img src="${rows[i].image}"></a>
-            </div>
-         </div>
-          <div class="col-md-8">
-              <a href="${href}"><p>${rows[i].title}</p></a>
-              <small><strong>${rows[i].description}</strong></small>
+          <div class="images-news-title">
+            <h4><a href="readnews">${rows[i].title}</a></h4>
+          </div>
+
+          <div class="images-new-describle">
+            <small style="color:#888">${rows[i].description}</small>
           </div>
         </div>
-      `;
+        `;
+      }
+      else
+      {
+        options.news += `
+        <div class="row news-row">
+            <div class="col-md-4">
+              <div class="news-row-img">
+                <a href="#"><img width="200" src="${rows[i].image}" alt="${rows[i].title}" /></a>
+              </div>
+            </div>
+            <div class="col-md-8">
+              <div class="news-row-box">
+                <div class="news-row-title">
+                  <a href="#"><h4>${rows[i].title}</h4></a>
+                </div>
+                <div class="news-row-content">
+                  <p style="color:#888">
+                    ${rows[i].description}
+                  </p>
+                </div>
+              </div>
+            </div>
+        </div>
+        `;
+      }
     }
+  })
+  .find({}, 'newsfeed', {sort: {datecreate: -1}, limit: 2})
+  .handle((rows) => {
+    options.newest = '';
+
+    rows.forEach((row, id) => {
+      options.newest += `
+      <div class="row _news-row-slider">
+        <div class="col-md-4">
+          <div class="_news-row-slider-img">
+            <a href="#newslist">
+              <img src="${row.image}" alt="${row.title}" width="138" />
+            </a>
+          </div>
+        </div>
+        <div class="col-md-8">
+          <div class="_news-row-slider-title">
+            <a href="#newslist">
+              <p>${row.title}</p>
+            </a>
+          </div>
+        </div>
+      </div>
+      `;
+    });
   })
   .close(() => {
     res.render('newslist', options);
