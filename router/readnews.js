5d7ab824f67c0769547f787c3aba8a51043c76b0
thanhan7914 <thanhan7914@gmail.com>
2016-11-21 23:36:57 +0700

    done :)) readnews

--- router/readnews.js
+++ router/readnews.js
@@ -14,6 +14,33 @@ router.get('/readnews/([a-zA-Z0-9_\-]+)\.([a-z0-9]{24})', function(req, res) {
 
     utils.clonewith(rows[0], options, ['title', 'content', 'datecreate', 'description', 'author']);
   })
+  .find({}, 'newsfeed', {sort: {datecreate: -1}, limit: 8})
+  .handle((rows) => {
+    options.newest = '';
+
+    rows.forEach((row, id) => {
+      let href = `/readnews/${utils.toUrl(row.title)}.${row._id}`;
+
+      options.newest += `
+      <div class="row _news-row-slider">
+        <div class="col-md-4">
+          <div class="_news-row-slider-img">
+            <a href="${href}">
+              <img src="${row.image}" alt="${row.title}" width="120" />
+            </a>
+          </div>
+        </div>
+        <div class="col-md-8">
+          <div class="_news-row-slider-title">
+            <a href="${href}">
+              <p>${row.title}</p>
+            </a>
+          </div>
+        </div>
+      </div>
+      `;
+    });
+  })
   .except((error) => {
     console.log(error);
   })
