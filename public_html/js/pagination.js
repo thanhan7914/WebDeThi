6720d7aaf3258c632b01eefb51521ec33e880860
PHAN THANH AN <thanhan7914@gmail.com>
2017-11-12 22:21:34 +0700

    $Pagination

--- public_html/js/pagination.js
+++ public_html/js/pagination.js
@@ -1,59 +1,75 @@
 /**
-* @author Thanh An
-* @datetime 15/11/2016
-**/
+ * @author Thanh An
+ * @date 15/11/2016
+ * @edit 12/11/2017
+ **/
 
-function $Pagination(pagination, pages, curpage) {
-  if(typeof pages !== 'number') pages = 0;
-  if(typeof curpage != 'number') curpage = 0;
-  this.pages = pages;
-  this.curpage = curpage;
-  this.pagination = pagination;
+function $Pagination(pagination, pages, curpage, tag = 'page', sufix = '') {
+    if (typeof pages !== 'number') pages = 0;
+    if (typeof curpage != 'number') curpage = 1;
+    if (curpage <= 0) curpage = 1;
+    this.pages = pages;
+    this.curpage = curpage;
+    this.pagination = pagination;
+    this.tag = tag;
+    this.sufix = sufix;
+    this.mark = [];
+    this.addPag(1);
+    this.addPag(2);
+    this.addPag(3);
+    this.addPag(curpage - 2);
+    this.addPag(curpage - 1);
+    this.addPag(curpage);
+    this.addPag(curpage + 1);
+    this.addPag(curpage + 2);
+    this.addPag(pages - 2);
+    this.addPag(pages - 1);
+    this.addPag(pages);
+    for (let i = 0; i < this.mark.length - 1; i++) {
+        if (this.mark[i] < this.mark[i + 1] - 1) {
+            this.mark.splice(i + 1, 0, -1);
+            i++;
+        }
+    }
 }
+
+$Pagination.prototype.addPag = function(p) {
+    if (p > 0 && p <= this.pages && this.mark.indexOf(p) == -1)
+        this.mark.push(p);
+};
 
 $Pagination.prototype.init = function init() {
-  let pre = this.curpage - 1;
-  let next = this.curpage + 1;
-  pre = pre < 1 ? 1: pre;
-  next = next > this.pages ? this.pages : next;
-
-  this.showPrev();
-  this.showThreeDot(this.curpage - 3);
-  this.show(this.curpage - 2);
-  this.show(this.curpage - 1);
-  this.showCurPage();
-  this.show(this.curpage + 1);
-  this.show(this.curpage + 2);
-  this.showThreeDot(this.curpage + 3);
-  this.showNext();
+    this.showPrev();
+    this.mark.forEach((i) => {
+        if (i === -1)
+            this.showThreeDot();
+        else
+            this.show(i);
+    });
+    this.showNext();
 }
 
-$Pagination.prototype.showThreeDot = function showThreeDot(page) {
-  if(page < this.pages && page > 1)
+$Pagination.prototype.showThreeDot = function showThreeDot() {
     this.pagination.append('<li><a href="javascript:void(0)">...</a></li>');
 }
 
 $Pagination.prototype.show = function show(page) {
-  if(page <= this.pages && page >= 1)
-    this.pagination.append('<li><a href="?page=' + page + '">'+ page +'</a></li>');
-}
-
-$Pagination.prototype.showCurPage = function showCurPage() {
-  this.pagination.append('<li><a class="active" href="?page=' + this.curpage + '">'+ this.curpage +'</a></li>');
+    if (page <= this.pages && page >= 1)
+        this.pagination.append('<li' + (page === this.curpage ? ' class="active"' : '') + '><a href="?' + this.tag + '=' + page + this.sufix + '">' + page + '</a></li>');
 }
 
 $Pagination.prototype.showPrev = function showPrev() {
-  let prev = this.curpage - 1;
-  if(prev >= 1)
-      this.pagination.append('<li><a href="?page=' + prev + '">«</a></li>');
-  else
-      this.pagination.append('<li class="disabled"><a href="javascript:void(0)">«</a></li>');
+    var prev = this.curpage - 1;
+    if (prev >= 1)
+        this.pagination.append('<li><a href="?' + this.tag + '=' + prev + this.sufix + '">«</a></li>');
+    else
+        this.pagination.append('<li class="disabled"><a href="javascript:void(0)">«</a></li>');
 }
 
 $Pagination.prototype.showNext = function showNext() {
-  let next = this.curpage + 1;
-  if(next <= this.pages)
-      this.pagination.append('<li><a href="?page=' + next + '">»</a></li>');
-  else
-      this.pagination.append('<li class="disabled"><a href="javascript:void(0)">»</a></li>');
+    var next = this.curpage + 1;
+    if (next <= this.pages)
+        this.pagination.append('<li><a href="?' + this.tag + '=' + next + this.sufix + '">»</a></li>');
+    else
+        this.pagination.append('<li class="disabled"><a href="javascript:void(0)">»</a></li>');
 }
